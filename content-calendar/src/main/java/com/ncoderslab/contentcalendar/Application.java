d90d0cca95d0a8b1d432113dbc234066d5c33cd5
Sumanta <sumanta@ncoderslab.in>
2023-12-18 23:48:24 +0530

    Implement data initilier using CommandLineRunner

--- content-calendar/src/main/java/com/ncoderslab/contentcalendar/Application.java
+++ content-calendar/src/main/java/com/ncoderslab/contentcalendar/Application.java
@@ -1,9 +1,16 @@
 package com.ncoderslab.contentcalendar;
 
+import com.ncoderslab.contentcalendar.model.Content;
+import com.ncoderslab.contentcalendar.model.Status;
+import com.ncoderslab.contentcalendar.model.Type;
+import com.ncoderslab.contentcalendar.repository.ContentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -16,6 +23,15 @@ public class Application {
         int beanDefinitionCount = ctx.getBeanDefinitionCount();
         System.out.println(beanDefinitionCount);
 
+    }
+
+    @Bean
+    public CommandLineRunner dbInitializer(ContentRepository contentRepository) {
+        System.out.println("I am from dbInitializer");
+        return args -> {
+            Content content = new Content(null, "My init Blog post", "Description", Status.COMPLETED, Type.ARTICLE, LocalDateTime.now(), null, "");
+            //contentRepository.save(content);
+        };
     }
 
 }
